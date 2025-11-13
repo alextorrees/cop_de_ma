a2e8299e5a0769096bd059244a88510aec45bc83
Your Name <you@example.com>
2025-11-13 23:50:19 +0100

    trailingSlash add

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const repoName = "cop_de_ma"; // <-- pon EXACTAMENTE tu nombre de repo en GitHub
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,        // 👈 IMPORTANTE PARA GITHUB PAGES
   images: {
     unoptimized: true,
   },
